7657a9c9cc8b42ea7fac785520c31a8d59231714
수먀 <ks670711@gmail.com>
2022-05-05 23:14:15 +0900

    Commit

---
+++ src/main/java/com/scon/project/admin/academy/model/service/AcademyService.java
@@ -0,0 +1,10 @@
+package com.scon.project.admin.academy.model.service;
+
+import com.scon.project.admin.academy.model.dto.AcademyDTO;
+
+public interface AcademyService {
+	
+	AcademyDTO selectAcademyInfo();
+	
+
+}
