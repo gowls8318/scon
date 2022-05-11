0d7e5807a598d2b8acabe4473b253b5dc0aeab19
YeonjeeSong <yeonjee.song@gmail.com>
2022-05-11 22:23:42 +0900

    commit

--- src/main/java/com/scon/project/admin/lecture/model/dao/LectureMapper.java
+++ src/main/java/com/scon/project/admin/lecture/model/dao/LectureMapper.java
@@ -6,27 +6,21 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.scon.project.admin.Class.dto.ClassDTO;
 import com.scon.project.admin.lecture.model.dto.LectureDTO;
+import com.scon.project.member.model.dto.MemberDTO;
 
 @Mapper
 public interface LectureMapper {
 
-	/* 수강 내역 조회용 */
+	/* 강의 내역 조회용 */
 	List<LectureDTO> selectAllLectureList();
+	
+	/* 강의 조회용 */
+	List<ClassDTO> selectAllClassList();
 
-	/* 수강 등록 - 전체 강의 조회용 */
-	List<ClassDTO> findAllClassList();
-
-	/* 수강 등록 - 강의 정보 조회용 */
-	List<ClassDTO> findClassDetail();
+	/* 원생 조회용 */
+	List<MemberDTO> selectAllMemberList();
 	
 	/* 수강 등록용 */
 	int insertLecture(LectureDTO lec);
-
-	/* 수강 상세 조회용 */
-	LectureDTO selectLectureDetail(int no);
-	
-	/* 수강 수정용 */
-	int modifyLecture(LectureDTO lec);
-
 
 }
--- src/main/java/com/scon/project/admin/lecture/model/service/LectureService.java
+++ src/main/java/com/scon/project/admin/lecture/model/service/LectureService.java
@@ -4,25 +4,20 @@ import java.util.List;
 
 import com.scon.project.admin.Class.dto.ClassDTO;
 import com.scon.project.admin.lecture.model.dto.LectureDTO;
+import com.scon.project.member.model.dto.MemberDTO;
 
 public interface LectureService {
 
 	/* 수강 내역 조회용 */
 	List<LectureDTO> selectAllLectureList() throws Exception;
+	
+	/* 강의 조회용 */
+	List<ClassDTO> selectAllClassList() throws Exception;
 
-	/* 수강 등록 - 전체 강의 조회용 */
-	List<ClassDTO> findAllClassList() throws Exception;
-
-	/* 수강 등록 - 강의 정보 조회용 */
-	List<ClassDTO> findClassDetail() throws Exception;
+	/* 원생 조회용 */
+	List<MemberDTO> selectAllMemberList() throws Exception;
 	
 	/* 수강 등록용 */
 	int insertLecture(LectureDTO lec) throws Exception;
-
-	/* 수강 상세 조회용 */
-	LectureDTO selectLectureDetail(int no) throws Exception;
-	
-	/* 수강 수정용 */
-	int modifyLecture(LectureDTO lec) throws Exception;
 
 }
