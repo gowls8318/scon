74c7c5e91aac74308d44911e5a4a4a5615942afb
hyejin kwon <kwonhyejin126@gmail.com>
2022-05-15 23:11:24 +0900

    noticePaging

--- src/main/java/com/scon/project/admin/notice/model/service/NoticeService.java
+++ src/main/java/com/scon/project/admin/notice/model/service/NoticeService.java
@@ -3,18 +3,11 @@ package com.scon.project.admin.notice.model.service;
 import java.util.List;
 
 import com.scon.project.admin.notice.model.dto.NoticeDTO;
-import com.scon.project.admin.notice.paging.Criteria;
 
 public interface NoticeService {
 
 //	공지 목록 조회
-//	List<NoticeDTO> findAllNoticeList() throws Exception;
-	
-//	목록+페이징
-	List<NoticeDTO> findAllNoticeList(Criteria cri) throws Exception;
-	
-//	게시물 총 갯수
-//	int allNoticeCount() throws Exception;
+	List<NoticeDTO> findAllNoticeList() throws Exception;
 	
 	
 //	공지 게시글 등록
