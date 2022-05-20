8a26884849958151dfdb6998bfe0e65a29e88d4d
수먀 <ks670711@gmail.com>
2022-05-21 00:08:16 +0900

    commit

--- src/main/java/com/scon/project/admin/check/model/service/CheckService.java
+++ src/main/java/com/scon/project/admin/check/model/service/CheckService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.scon.project.admin.Class.dto.ClassDTO;
 import com.scon.project.admin.check.model.dto.CheckDTO;
+import com.scon.project.admin.lecture.model.dto.LectureDTO;
 import com.scon.project.member.model.dto.MemberDTO;
 
 public interface CheckService {
@@ -11,10 +12,19 @@ public interface CheckService {
 	/* 날짜별 반 출석 조회 */
 	List<CheckDTO> selectAllClassList(ClassDTO cls, CheckDTO chk, MemberDTO member) throws Exception;
 	
-	/* 학생별 출석 조회 */
-	List<CheckDTO> selectAllStudentList() throws Exception;
+	/* 결석 학생 조회 */
+	List<CheckDTO> selectAllAbsentList(ClassDTO cls, CheckDTO chk, MemberDTO member) throws Exception;
 	
 	/* 강의 목록 조회*/
 	List<ClassDTO> selectClassList(ClassDTO cls) throws Exception;
 
+	/* 출석 등록 목록 불러오기 */
+	List<LectureDTO> selectChkList(ClassDTO cls, LectureDTO lec, MemberDTO member) throws Exception;
+
+	/* 출석 등록하기 */
+	int insertChkList(CheckDTO chk) throws Exception;
+	
+	/* 출석 수정하기 */
+	int updateChkList(CheckDTO chk) throws Exception;
+	
 }
