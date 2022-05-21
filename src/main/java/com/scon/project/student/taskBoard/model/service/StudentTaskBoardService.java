702d84be559b7c8889f1f8ce079759f9c606f2da
jayeon kim <jyk10045@gmail.com>
2022-05-21 23:07:22 +0900

    commit

--- src/main/java/com/scon/project/student/taskBoard/model/service/StudentTaskBoardService.java
+++ src/main/java/com/scon/project/student/taskBoard/model/service/StudentTaskBoardService.java
@@ -1,5 +1,19 @@
 package com.scon.project.student.taskBoard.model.service;
 
+import java.util.List;
+
+import com.scon.project.admin.taskBoard.model.dto.TaskBoardDTO;
+import com.scon.project.common.paging.Criteria;
+
+
 public interface StudentTaskBoardService {
+
+//	int total(TaskBoardDTO taskBoard);
+//
+//	List<TaskBoardDTO> findAllStudentTask(TaskBoardDTO taskBoard, Criteria cri);
+
+	
+
+
 
 }
