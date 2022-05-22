aa11187c25e710e7254727b5a9b304abd9eb12ec
hyejin kwon <kwonhyejin126@gmail.com>
2022-05-22 23:53:48 +0900

    check

--- src/main/java/com/scon/project/admin/notice/controller/NoticeController.java
+++ src/main/java/com/scon/project/admin/notice/controller/NoticeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.scon.project.admin.notice.model.dto.NoticeCmtDTO;
 import com.scon.project.admin.notice.model.dto.NoticeDTO;
